d1c24ff57bf88c5ae6cc3272e753fb89cad40ee2
JEFF0019 <JEFF0019@e.ntu.edu.sg>
2020-05-26 09:25:13 -0400

    First commit

---
+++ src/resumeData.js
@@ -0,0 +1,130 @@
+let resumeData = {
+  imagebaseurl: "https://rbhatia46.github.io/",
+  name: "Jeffery Cao",
+  role:
+    "4th year undergraduate from Nanyang Technological University with experiences in research, data science and software development",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  headerDescription: "",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/jeffery-cao-siming/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/jeffery1236",
+      className: "fa fa-github",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/jeffery.y.y.y/",
+      className: "fa fa-instagram",
+    },
+  ],
+  aboutme:
+    "I am currently a final year undergraduate from Nanyang Technological University with experiences in research, data science and software development. Currently, I am looking for a full time role when I graduate in 2021. Feel free to connect with me on linkedin!",
+  address: "Jurong East Street 31, Singapore",
+  email: "JEFF0019@e.ntu.edu.sg",
+  education: [
+    {
+      UniversityName: "Nanyang Technological University",
+      specialization: "Electrical and Electronic Engineering",
+      MonthOfPassing: "Jun",
+      YearOfPassing: "2021",
+      Achievements: ["- CGPA: 4.91/5.0", "- Dean's list 2018/19"],
+    },
+    {
+      UniversityName: "University of Waterloo",
+      specialization: "Semester Exchange",
+      MonthOfPassing: "Dec",
+      YearOfPassing: "2019",
+      Achievements: [""],
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Safemode, Tel Aviv, Israel",
+      specialization: "Data Science Intern",
+      MonthOfLeaving: "April",
+      YearOfLeaving: "2020",
+      Achievements: [
+        "- Enabled efficient behaviorial changes by developing predictive models for drivers' behavior",
+      ],
+    },
+    {
+      CompanyName: "Ramco Systems Ltd, Innovation Lab, Singapore",
+      specialization: "Software Engineering Intern",
+      MonthOfLeaving: "Aug",
+      YearOfLeaving: "2019",
+      Achievements: [
+        "- Developed a computer vision model for facial recognition liveness detection",
+        "- Initiated potential partnership with Zoloz, chinese facial recognition startup",
+      ],
+    },
+    {
+      CompanyName: "Nanyang Technological University, Singapore",
+      specialization: "Undergraduate Researcher",
+      MonthOfLeaving: "Aug",
+      YearOfLeaving: "2019",
+      Achievements: [
+        "- Designed a momentum-based learning algorithm for Spiking Neural Networks",
+        "- Improved convergence rates and accuracy; Authored research paper documenting results",
+      ],
+    },
+    {
+      CompanyName: "Société Générale, Singapore",
+      specialization: "Student Consultant",
+      MonthOfLeaving: "Mar",
+      YearOfLeaving: "2019",
+      Achievements: [
+        "- Worked with VP of Talent Development to develop effective learning solutions",
+        "- Achieved 2nd Runners up and title of 'Most Promising Leader'; 2 proposals were implemented by Singapore's department",
+      ],
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description:
+        "During his Data Science internship at Safemode, Jeffery has shown to be a very talented professional with strong analytical skills and very good understanding of the required mathematical, machine learning and Python tools. Most important, he has always shown to be a great team player, eager to learn, very curious and strongly commited to bring value to the company. I have really enjoyed working with him and have no doubt that Jeffery will be a great Data Scientist.",
+      name: "Marcelo, CTO of Safemode",
+    },
+  ],
+};
+
+export default resumeData;
